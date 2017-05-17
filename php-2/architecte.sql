8ac0397d8ef4699efc0c0667fb53ceafa6d9206e
alisonsayarath <alison.sayarath@hetic.net>
2017-05-17 16:24:31 +0200

    structure bdd complete

--- php-2/architecte.sql
+++ php-2/architecte.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 17 Mai 2017 à 14:36
+-- Généré le :  Mer 17 Mai 2017 à 16:24
 -- Version du serveur :  5.5.42
--- Version de PHP :  5.6.10
+-- Version de PHP :  7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,83 +17,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `articles`
+-- Structure de la table `orders`
 --
 
-CREATE TABLE `articles` (
-  `id` int(255) NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `architect` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `main_img` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `second_image` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `contributors` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `resume` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `journals`
---
-
-CREATE TABLE `journals` (
-  `id` int(255) NOT NULL,
-  `number` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `date` date NOT NULL,
-  `img` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `region` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `slug` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `journals`
---
-
-INSERT INTO `journals` (`id`, `number`, `date`, `img`, `region`, `slug`) VALUES
-(9, 'ruben', '0000-00-00', 'jerem.jpeg', 'attali', '');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(255) NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `email`) VALUES
-(1, 'lolo', 'lele', 'lolo@gmail.com'),
-(2, 'admin', '1234', 'admin');
 
 --
 -- Index pour les tables exportées
 --
 
 --
--- Index pour la table `articles`
+-- Index pour la table `orders`
 --
-ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `journals`
---
-ALTER TABLE `journals`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -101,17 +45,7 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT pour la table `articles`
+-- AUTO_INCREMENT pour la table `orders`
 --
-ALTER TABLE `articles`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `journals`
---
-ALTER TABLE `journals`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
