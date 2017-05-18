a5f9ec69d292d1906c1c848fcbdbd7ee252dd110
jeremiebergel <jeremie.bergel@hetic.net>
2017-05-18 10:50:01 +0200

    base de donnée new

--- php/architecte.sql
+++ php/architecte.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost:3306
--- Généré le :  Mer 17 Mai 2017 à 16:24
+-- Généré le :  Jeu 18 Mai 2017 à 10:47
 -- Version du serveur :  5.5.42
--- Version de PHP :  7.0.8
+-- Version de PHP :  5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -27,7 +27,12 @@ CREATE TABLE `orders` (
   `address` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `mail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `function` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `activity` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `social` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fax` int(255) NOT NULL,
+  `phone` int(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
