dccd6dc1dc11b7306995ad783231afbd7ccc46ed
alisonsayarath <alison.sayarath@hetic.net>
2017-05-18 09:58:45 +0200

    no more conflicts

--- php-2/CRUD/class.crud.php
+++ php-2/CRUD/class.crud.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: jeremiebergel
@@ -9,98 +8,78 @@
 class crud
 {
     private $db;
-
-    public function __construct(\PDO $db_con)
+    function __construct(\PDO $db_con)
     {
         $this->db = $db_con;
     }
-
-    public function create($number, $date, $img, $region)
+    public function create ($number, $date, $img, $region, $synopsis)
     {
-        try {
-            $stmt = $this->db->prepare("INSERT INTO journals(`number`, `date`, `img`, `region`) VALUES (:number, :date, :img, :region)");
-            $stmt->bindparam(":number", $number);
-            $stmt->bindparam(":date", $date);
-            $stmt->bindparam(":img", $img);
-            $stmt->bindparam(":region", $region);
-            $stmt->execute();
-
-            return true;
-        } catch (PDOException $e) {
+        try
+        {
+         $stmt = $this->db->prepare("INSERT INTO journals(`number`, `date`, `img`, `region`, `synopsis`) VALUES (:number, :date, :img, :region, :synopsis)");
+         $stmt ->bindvalue(":number", $number);
+         $stmt ->bindvalue(":date", $date);
+         $stmt ->bindvalue(":img", $img);
+         $stmt ->bindvalue(":region", $region);
+         $stmt ->bindvalue(":synopsis", $synopsis);
+         $stmt ->execute();
+         return true;
+        }
+        catch(PDOException $e)
+        {
             echo $e->getMessage();
             return false;
         }
-
     }
-
     public function getID($id)
     {
         $stmt = $this->db->prepare("SELECT * FROM journals WHERE id=:id");
-        $stmt->execute(array(":id" => $id));
-        $editRow = $stmt->fetch(PDO::FETCH_ASSOC);
+        $stmt->execute(array(":id"=>$id));
+        $editRow=$stmt->fetch(PDO::FETCH_ASSOC);
         return $editRow;
     }
-
-    public function update($id, $number, $date, $img, $region)
+    public function update ($id, $number, $date, $img, $region, $synopsis)
     {
-        try {
-            $query = "UPDATE journals SET `number`=:number, `date`=:date, img=:img, region=:region WHERE id=:id";
-
+        try
+        {
+            $query= "UPDATE journals SET `number`=:number, `date`=:date, `img`=:img, `region`=:region, `synopsis`=:synopsis WHERE id=:id";
             $data = ['id' => $id,
-                'number' => $number,
-                'date' => $date,
-                'img' => $img,
-                'region' => $region];
-
+                    'number' => $number,
+                    'date' => $date,
+                    'img' => $img,
+                    'region' => $region,
+                    'synopsis' => $synopsis];
             if ($this->bind($query, $data)->rowCount()) {
-                var_dump('youhoooouuu');
-
                 return true;
             }
-
             die('GROS BATAAAARD');
-
-//            $stmt->bindvalue(":number",$number);
-//            $stmt->bindvalue(":date",$date);
-//            $stmt->bindvalue(":img",$img);
-//            $stmt->bindvalue(":region",$region);
-//            $stmt->bindvalue(":id",$id);
-//            $stmt->execute();
-
-
-        } catch (PDOException $e) {
+        }
+        catch(PDOException $e)
+        {
             echo $e->getMessage();
             return false;
         }
     }
-
-
     public function delete($id)
     {
         $stmt = $this->db->prepare("DELETE FROM journals WHERE id=:id");
-        $stmt->bindvalue(":id", $id);
+        $stmt->bindvalue(":id",$id);
         $stmt->execute();
-//        return true;
+        return true;
     }
-
     public function dataview($query)
     {
         $stmt = $this->db->prepare($query);
         $stmt->execute();
-
         return $stmt;
-
     }
-
     public function bind($query, $data)
     {
         $stmt = $this->db->prepare($query);
         foreach ($data as $key => $value) {
-            $stmt->bindValue(':' . $key, $value);
+            $stmt->bindValue(':'.$key, $value);
         }
         $stmt->execute();
-
         return $stmt;
     }
-
 }
