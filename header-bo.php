dc6793f41702aa34adda9496b619fced623f4f08
alisonsayarath <alison.sayarath@hetic.net>
2017-05-18 20:17:35 +0200

    the very last commit

--- header-bo.php
+++ header-bo.php
@@ -11,11 +11,6 @@
 <body id="bo" class="modify">
 
 <header class="bo_header">
-  <div class="logo">
-    <a href="index.php">
-      <img src="assets/img-layout/logo.svg" alt="Logo de Architecture des Régions">
-    </a>
-  </div>
     <div class="links">
         <span>Espace administrateur</span>
     </div>
